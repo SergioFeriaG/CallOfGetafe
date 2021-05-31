c50af26e93ec08e794025555d658bd4bc44ec71d
SergioFeriaG <83406793+SergioFeriaG@users.noreply.github.com>
2021-05-31 14:19:37 +0200

    upload310521

---
+++ Assets/_GameAssets/Scripts/Enemies/PatrolManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolManager : MonoBehaviour
+{
+    public float walkSpeed;//Velocidad de andar
+    public float runSpeed;//Velocidad de correr
+    public float runDistance;//Distancia a partir de la cual se pone a correr el AI
+    public float idleTime=3;//Tiempo que est· Axel esperando en un patrolPoint antes de continuar
+    public bool isPlayerDetected = false;//Indica si ha detectado al player
+    public bool randomRoute;//Indica si la ruta es aleatoria
+    public Transform[] patrolPoints;//Puntos de la ruta
+    private NavMeshAgent nma;//Sistema de navegaciÛn
+    public int currentPoint = 0;//Indica el siguiente patrolPoint a seguir
+    private Animator agentAnimator;//Animator controller
+    private bool hasDestination = false;
+
+    private void Awake()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        agentAnimator = GetComponentInChildren<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma.SetDestination(patrolPoints[currentPoint].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nma.remainingDistance <= nma.stoppingDistance+0.1f)
+        {
+            if (!hasDestination)
+            {
+                agentAnimator.SetBool("Walk", false);
+                Invoke("SetNewDestination", idleTime);
+                hasDestination = true;
+            }
+        } else
+        {
+            if (nma.remainingDistance > runDistance)
+            {
+                agentAnimator.SetBool("Run", true);
+                nma.speed = runSpeed;
+            } else
+            {
+                agentAnimator.SetBool("Run", false);
+                agentAnimator.SetBool("Walk", true);
+                nma.speed = walkSpeed;
+            }
+        }
+    }
+
+    private void SetNewDestination()
+    {
+        hasDestination = false;
+        currentPoint++;
+        if (currentPoint == patrolPoints.Length) currentPoint = 0;
+        if (randomRoute)
+        {
+            nma.SetDestination(patrolPoints[Random.Range(0, patrolPoints.Length)].transform.position);
+        }
+        else
+        {
+            nma.SetDestination(patrolPoints[currentPoint].transform.position);
+        }
+    }
+
+    public void SetExternalDestination(Vector3 target)
+    {
+        nma.SetDestination(target);
+    }
+}
