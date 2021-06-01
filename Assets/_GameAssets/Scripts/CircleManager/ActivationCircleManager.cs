eef0205ce1276fb6151628fc68f62a043d59cb14
SergioFeriaG <83406793+SergioFeriaG@users.noreply.github.com>
2021-06-01 22:39:22 +0200

    updater

---
+++ Assets/_GameAssets/Scripts/CircleManager/ActivationCircleManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationCircleManager : MonoBehaviour
+{
+    //Opci�n 1.
+    public Animator animator;
+    private AudioSource audioSource;
+    public AudioClip deepHorn;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();    
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("Open", true);
+            PlayHornSound();
+        }
+    }
+    private void PlayHornSound()
+    {
+        audioSource.PlayOneShot(deepHorn);
+    }
+    //OpciÛn 2.
+    /*
+    private Animator animatorPuerta;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            animatorPuerta = GameObject.Find("Door").GetComponent<Animator>();
+            animatorPuerta.enabled = true;
+        }
+    }
+    */
+}
