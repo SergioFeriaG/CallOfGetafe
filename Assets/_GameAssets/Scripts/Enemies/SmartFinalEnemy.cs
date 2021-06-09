20297e7927ae3298fc36ece63ee3208554481d09
SergioFeriaG <83406793+SergioFeriaG@users.noreply.github.com>
2021-06-09 23:59:10 +0200

    Update_Final_Enemy_Anim

---
+++ Assets/_GameAssets/Scripts/Enemies/SmartFinalEnemy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartFinalEnemy : MobileEnemy {
+
+    [Range(1,50)]
+    public float followDistance;
+
+    public override void Update()
+    {
+        base.Update();//Ejecuta la implementaci n de Update de la clase base
+        if (distanceToPlayer <= followDistance)
+        {
+            Vector3 target = new Vector3(player.transform.position.x, transform.position.y ,player.transform.position.z);
+            transform.LookAt(target);
+        } 
+        Move();
+    }
+    public override void Rotate()
+    {
+        if (distanceToPlayer <= followDistance) return;
+        base.Rotate();
+    }
+}
